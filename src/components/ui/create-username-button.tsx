ae180c1aee5093fe82853203cdd8100f9a99e368
Allan Severo <grupoteaser@gmail.com>
2025-07-30 14:14:37 +0000

    Ao clicar em feito no botão da página pública o usuário tem que ser envi

--- src/components/ui/create-username-button.tsx
+++ src/components/ui/create-username-button.tsx
@@ -93,11 +93,11 @@ export const CreateUsernameButton = () => {
       onClick={handleButtonClick}
       className={cn(
         "group relative flex h-12 items-center justify-center overflow-hidden rounded-2xl bg-black text-white shadow-lg transition-all duration-300 ease-in-out",
-        isActive ? 'w-80' : 'w-auto px-6 hover:w-64 cursor-pointer'
+        isActive ? 'w-80' : 'w-auto hover:w-64 cursor-pointer'
       )}
     >
       {!isActive ? (
-        <>
+        <div className="flex h-full items-center px-6">
             <span className="relative whitespace-nowrap opacity-100 transition-opacity duration-200 group-hover:opacity-0">
                 Pegue seu Nits
             </span>
@@ -105,7 +105,7 @@ export const CreateUsernameButton = () => {
                 <span className="font-medium">Crie seu&nbsp;</span>
                 <span className="font-bold">Nits.uno/</span>
             </div>
-        </>
+        </div>
       ) : (
         <div className="flex h-full w-full items-center p-1 gap-1">
             <span className="pl-2 pr-1 font-bold text-white">Nits.uno/</span>
